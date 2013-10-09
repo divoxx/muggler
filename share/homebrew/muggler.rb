1a994caecff5ad0aabf47a54e0ab0dc3d287b5b4
Rodrigo Kochenburger <divoxx@gmail.com>
2013-10-09 16:23:09 -0700

    Update homebrew formula to v0.1.1

--- share/homebrew/muggler.rb
+++ share/homebrew/muggler.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Muggler < Formula
   homepage 'https://github.com/divoxx/muggler'
-  url 'https://github.com/divoxx/muggler/archive/v0.1.0.tar.gz'
-  sha1 '9628d458a70ce9b5fa0a801f78f4455491e4a305'
+  url 'https://github.com/divoxx/muggler/archive/v0.1.1.tar.gz'
+  sha1 'eb32e14b5c529c231a175ce753713b7e21e61ff4'
 
   def install
     system "cp -R * \"#{prefix}/\""
